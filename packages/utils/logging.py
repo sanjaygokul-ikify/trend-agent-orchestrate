10d6812e7c3fecc31f9fe2cf3ef7be8b11574b77
Sanjay G <gokulesh2615@gmail.com>
2026-06-25 14:28:37 +0530

    feat(logging): add structured JSON logging with context propagation

---
+++ packages/utils/logging.py
@@ -0,0 +1,9 @@
+import logging
+
+def init_logger(name=None):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(handler)
+    return logger
